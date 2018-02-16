ff95eca8ec766fc97533859c6f4cdeec2a81c468
rosexw <rose.wan@evermight.com>
2018-02-15 23:08:33 -0500

    add console.log messages to buttons and tabs clicked

--- app/pods/products/index/controller.js
+++ app/pods/products/index/controller.js
@@ -2,5 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   publisher: Ember.computed.alias('model.publisher'),
-  products: Ember.computed.alias('model.products')
+  products: Ember.computed.alias('model.products'),
+  actions: {
+    pricing() {
+      console.log('pricing button clicked');
+    },
+    commercial() {
+      console.log('commercial data button clicked');
+    },
+    menu() {
+      console.log('menu hamburger button pressed');
+    },
+    logo() {
+      console.log('logo pressed');
+    },
+    tab() {
+      console.log('tab pressed');
+    }
+  }
 });
--- app/pods/products/show/controller.js
+++ app/pods/products/show/controller.js
@@ -4,4 +4,9 @@ export default Ember.Controller.extend({
   publisher: Ember.computed.alias('model.publisher'),
   product: Ember.computed.alias('model.product'),
   productData: Ember.computed.alias('product.data'),
+  actions: {
+    tab() {
+      console.log('tab pressed');
+    }
+  }
 });
